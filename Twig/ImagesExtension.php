11c9fe88632e8136d9a7c192a225a94e9e8bc5ef
Alexander Murashov <rodger@ladela.com>
2013-03-11 21:20:12 +0200

    add is_uploaded filter

--- Twig/ImagesExtension.php
+++ Twig/ImagesExtension.php
@@ -8,6 +8,7 @@ use \Twig_SimpleFunction;
 use Symfony\Component\DependencyInjection\Container;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 use Liip\ImagineBundle\Templating\Helper\ImagineHelper;
+use \InvalidArgumentException;
 
 class ImagesExtension extends Twig_Extension
 {
@@ -16,12 +17,12 @@ class ImagesExtension extends Twig_Extension
 
   public function __construct(Container $container)
   {
-    /** @var UploaderHelper $uploader_helper  */
+    /** @var UploaderHelper $uploader_helper */
     $uploader_helper = $container->get('vich_uploader.templating.helper.uploader_helper');
-    /** @var ImagineHelper $thumbnails_helper  */
+    /** @var ImagineHelper $thumbnails_helper */
     $thumbnails_helper = $container->get('liip_imagine.templating.helper');
 
-    $this->uploader_helper = $uploader_helper;
+    $this->uploader_helper   = $uploader_helper;
     $this->thumbnails_helper = $thumbnails_helper;
   }
 
@@ -29,6 +30,7 @@ class ImagesExtension extends Twig_Extension
   {
     return array(
       new Twig_SimpleFilter('uploaded_image', array($this, 'uploaded_image'), array('is_safe' => array('html'))),
+      new Twig_SimpleFilter('is_uploaded', array($this, 'is_uploaded')),
     );
   }
 
@@ -42,6 +44,21 @@ class ImagesExtension extends Twig_Extension
   public function call_entity_method($entity, $method)
   {
     return call_user_func(array($entity, $method));
+  }
+
+  /**
+   * Checks if file is uploaded
+   * @param $object
+   * @param string $property
+   * @return bool|string
+   */
+  public function is_uploaded($object, $property = 'file')
+  {
+    try {
+      return $this->uploader_helper->asset($object, $property);
+    } catch (InvalidArgumentException $e) {
+      return false;
+    }
   }
 
   /**
@@ -66,10 +83,10 @@ class ImagesExtension extends Twig_Extension
   protected function image_tag($src, $params = array())
   {
     $default_params = array('alt' => '');
-    $params = array_merge($default_params, $params);
-    $params['src'] = $src;
-    $result = array();
-    foreach($params as $key => $value) $result[] = strtr('%key%="%value%"', array('%key%' => $key, '%value%' => $value));
+    $params         = array_merge($default_params, $params);
+    $params['src']  = $src;
+    $result         = array();
+    foreach ($params as $key => $value) $result[] = strtr('%key%="%value%"', array('%key%' => $key, '%value%' => $value));
     return strtr("<img %params%/>", array('%params%' => implode(" ", $result)));
   }
 
