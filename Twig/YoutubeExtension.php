a2b007bafbc18fe23f7f324cf6183f0aaa26ae32
Alexander Murashov <rodger@ladela.com>
2013-12-20 13:23:34 +0200

    add youtube_embed_url filter

--- Twig/YoutubeExtension.php
+++ Twig/YoutubeExtension.php
@@ -14,63 +14,85 @@ use \Twig_SimpleFilter;
 
 class YoutubeExtension extends Twig_Extension
 {
+    const VIDEO_URL   = 'http://www.youtube.com/embed/%code%';
+    const IFRAME_CODE = '<iframe align="right" frameborder="0" scrolling="no" src="%url%"></iframe>';
 
-  public function getFilters()
-  {
-    return array(
-      new Twig_SimpleFilter('youtube_preview', array( $this, 'youtube_image_preview' ), array( 'is_safe' => array( 'html' ) )),
-      new Twig_SimpleFilter('youtube_embed', array( $this, 'youtube_embed' ), array( 'is_safe' => array( 'html' ) )),
-    );
-  }
+    public function getFilters()
+    {
+        return array(
+            new Twig_SimpleFilter('youtube_preview', array($this, 'youtube_image_preview'), array('is_safe' => array('html'))),
+            new Twig_SimpleFilter('youtube_embed', array($this, 'youtube_embed'), array('is_safe' => array('html'))),
+            new Twig_SimpleFilter('youtube_embed_url', array($this, 'youtube_embed_url'), array('is_safe' => array('html'))),
+        );
+    }
 
-  /**
-   * Renders youtube video preview image
-   * @param $url
-   * @param int $thumbnail
-   * @return bool|string
-   */
-  public function youtube_image_preview($url, $thumbnail = 0)
-  {
-    $code = $this->youtube_id($url);
+    /**
+     * Renders youtube video preview image
+     *
+     * @param     $url
+     * @param int $thumbnail
+     *
+     * @return bool|string
+     */
+    public function youtube_image_preview($url, $thumbnail = 0)
+    {
+        $code = $this->youtube_id($url);
 
-    return $code ? 'http://img.youtube.com/vi/' . $code . '/' . $thumbnail . '.jpg' : false;
-  }
+        return $code ? 'http://img.youtube.com/vi/' . $code . '/' . $thumbnail . '.jpg' : false;
+    }
 
-  /**
-   * Renders youtube embeded video
-   * @param $url
-   * @return bool|string
-   */
-  public function youtube_embed($url)
-  {
-    $string = <<< HTML
-    <iframe align="right" frameborder="0" scrolling="no" src="http://www.youtube.com/embed/%code%"></iframe>
-HTML;
+    /**
+     * Renders youtube embeded video
+     *
+     * @param $url
+     *
+     * @return bool|string
+     */
+    public function youtube_embed($url)
+    {
+        $url = $this->youtube_embed_url($url);
+        if (!$url) {
+            return false;
+        }
 
-    $code = $this->youtube_id($url);
+        return strtr(self::IFRAME_CODE, array('%url%' => $url));
+    }
 
-    return $code ? strtr($string, array( '%code%' => $code )) : false;
-  }
+    /**
+     * Gets embed url
+     *
+     * @param $url
+     *
+     * @return bool|string
+     */
+    public function youtube_embed_url($url)
+    {
+        $code = $this->youtube_id($url);
 
-  /**
-   * Parses youtube identificator from url
-   * @param $url
-   * @return bool
-   */
-  protected function youtube_id($url)
-  {
-    preg_match("/\/watch\?v=(?P<code>[\w\d]+)/", $url, $matches);
+        return $code ? strtr(self::VIDEO_URL, array('%code%' => $code)) : false;
+    }
 
-    return @$matches['code'] ? : false;
-  }
+    /**
+     * Parses youtube identificator from url
+     *
+     * @param $url
+     *
+     * @return bool
+     */
+    protected function youtube_id($url)
+    {
+        preg_match("/\/watch\?v=(?P<code>[\w\d]+)/", $url, $matches);
 
-  /**
-   * Returns the name of the extension.
-   *
-   * @return string The extension name
-   */
-  public function getName()
-  {
-    return 'youtube';
-  }
+        return @$matches['code'] ? : false;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'youtube';
+    }
 }
