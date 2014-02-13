ffa6496c312120bdc49ac1cfed1fca75d3bb1fe8
Alexander Murashov <rodger@ladela.com>
2014-02-13 15:50:03 +0200

    base functionality moved to a standalone manager

---
+++ Manager/ImagesManager.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodger
+ * Date: 13.02.14
+ * Time: 15:40
+ */
+
+namespace Rodgermd\SfToolsBundle\Manager;
+
+
+use Liip\ImagineBundle\Templating\Helper\ImagineHelper;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+use \InvalidArgumentException;
+
+/**
+ * Class ImagesManager
+ * Manages uploaded images
+ *
+ * @package Rodgermd\SfToolsBundle\Manager
+ */
+class ImagesManager
+{
+    /** @var \Vich\UploaderBundle\Templating\Helper\UploaderHelper */
+    protected $uploader;
+    /** @var \Liip\ImagineBundle\Templating\Helper\ImagineHelper */
+    protected $imagine;
+
+    /**
+     * Object constructor
+     *
+     * @param UploaderHelper $uploader
+     * @param ImagineHelper  $imagine
+     */
+    public function __construct(UploaderHelper $uploader, ImagineHelper $imagine)
+    {
+        $this->uploader = $uploader;
+        $this->imagine  = $imagine;
+    }
+
+    /**
+     * Gets image url
+     *
+     * @param mixed  $object
+     * @param string $filter
+     * @param string $property
+     * @param bool   $absolute
+     *
+     * @return bool
+     */
+    public function getImageUrl($object, $filter, $property, $absolute = false)
+    {
+        if (!$this->is_uploaded($object, $property)) {
+            return false;
+        }
+
+        $original_filename = $this->uploader->asset($object, $property);
+
+        return $this->imagine->filter($original_filename, $filter, $absolute);
+    }
+
+    /**
+     * Checks if file is uploaded
+     *
+     * @param        $object
+     * @param string $property
+     *
+     * @return bool|string
+     */
+    public function is_uploaded($object, $property = 'file')
+    {
+        if ($object === null) {
+            return false;
+        }
+        try {
+            return $this->uploader->asset($object, $property);
+        } catch (InvalidArgumentException $e) {
+            return false;
+        }
+    }
+} 
--- Twig/ImagesExtension.php
+++ Twig/ImagesExtension.php
@@ -2,33 +2,24 @@
 
 namespace Rodgermd\SfToolsBundle\Twig;
 
+use Rodgermd\SfToolsBundle\Manager\ImagesManager;
 use \Twig_Extension;
 use \Twig_SimpleFilter;
-use \Twig_SimpleFunction;
 use Symfony\Component\DependencyInjection\Container;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
-use Liip\ImagineBundle\Templating\Helper\ImagineHelper;
 use \InvalidArgumentException;
 
 class ImagesExtension extends Twig_Extension
 {
-    protected $uploader_helper;
-    protected $thumbnails_helper;
+    protected $manager;
 
     /**
      * Object constructor
      *
-     * @param Container $container
+     * @param \Rodgermd\SfToolsBundle\Manager\ImagesManager $manager
      */
-    public function __construct(Container $container)
+    public function __construct(ImagesManager $manager)
     {
-        /** @var UploaderHelper $uploader_helper */
-        $uploader_helper = $container->get('vich_uploader.templating.helper.uploader_helper');
-        /** @var ImagineHelper $thumbnails_helper */
-        $thumbnails_helper = $container->get('liip_imagine.templating.helper');
-
-        $this->uploader_helper   = $uploader_helper;
-        $this->thumbnails_helper = $thumbnails_helper;
+        $this->manager = $manager;
     }
 
     /**
@@ -39,9 +30,9 @@ class ImagesExtension extends Twig_Extension
     public function getFilters()
     {
         return array(
-          new Twig_SimpleFilter('uploaded_image', array($this, 'uploaded_image'), array('is_safe' => array('html'))),
-          new Twig_SimpleFilter('uploaded_image_source', array($this, 'uploaded_image_source'), array('is_safe' => array('html'))),
-          new Twig_SimpleFilter('is_uploaded', array($this, 'is_uploaded')),
+            new Twig_SimpleFilter('uploaded_image', array($this, 'uploaded_image'), array('is_safe' => array('html'))),
+            new Twig_SimpleFilter('uploaded_image_source', array($this, 'uploaded_image_source'), array('is_safe' => array('html'))),
+            new Twig_SimpleFilter('is_uploaded', array($this, 'is_uploaded')),
         );
     }
 
@@ -55,14 +46,7 @@ class ImagesExtension extends Twig_Extension
      */
     public function is_uploaded($object, $property = 'file')
     {
-        if ($object === null) {
-            return false;
-        }
-        try {
-            return $this->uploader_helper->asset($object, $property);
-        } catch (InvalidArgumentException $e) {
-            return false;
-        }
+        return $this->manager->is_uploaded($object, $property);
     }
 
     /**
@@ -77,7 +61,7 @@ class ImagesExtension extends Twig_Extension
      */
     public function uploaded_image($object, $filter, $property = 'file', $attributes = array())
     {
-        if (!$this->is_uploaded($object, $property)) {
+        if (!$this->manager->is_uploaded($object, $property)) {
             return false;
         }
 
@@ -87,8 +71,7 @@ class ImagesExtension extends Twig_Extension
             unset($attributes['absolute']);
         }
 
-
-        return $this->image_tag($this->uploaded_image_source($object, $filter, $property, $absolute), $attributes);
+        return $this->image_tag($this->manager->getImageUrl($object, $filter, $property, $absolute), $attributes);
     }
 
     /**
@@ -103,12 +86,7 @@ class ImagesExtension extends Twig_Extension
      */
     public function uploaded_image_source($object, $filter, $property = 'file', $absolute = false)
     {
-        if (!$this->is_uploaded($object, $property)) {
-            return false;
-        }
-        $original_filename = $this->uploader_helper->asset($object, $property);
-
-        return $this->thumbnails_helper->filter($original_filename, $filter, $absolute);
+        return $this->manager->getImageUrl($object, $filter, $property, $absolute);
     }
 
     /**
